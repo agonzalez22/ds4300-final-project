70fa721d4fc45dcb58bf5aed1219f74ec4d1d912
Alina <55455721+agonzalez22@users.noreply.github.com>
2025-04-18 17:53:10 -0400

    testing the rds

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from s3 import ingest_to_s3
 from io import StringIO
+from rds import give_status
 
 
 def move_next(): 
@@ -13,7 +14,9 @@ st.header("Survey Analytics")
 f = st.file_uploader("Upload your survey results in a CSV below", on_change=move_next)
 
 if f: # only if f is real :3
-    ingest_to_s3(f.name, f, "ds4300-edited-slicedbread")
+    ingest_to_s3(f.name, f, "ds4300-raw-bucket-test")
+    give_status()
+    # ingest_to_s3(f.name, f)
     
 
 
---
+++ rds.py
@@ -0,0 +1,19 @@
+import pymysql 
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = pymysql.connect(
+            host=os.getenv("AWS_HOST"),
+            port=3306, # hm
+            user=os.getenv("AWS_USER"),
+            password= os.getenv("AWS_ACCESS_KEY_ID"),
+            autocommit=True
+        )
+
+
+
+
+def give_status(): 
+    print(conn)
